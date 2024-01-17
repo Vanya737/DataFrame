211ff5525b66cfa6bffb7843d839097fe55b28cc
Ваня <144817452+Vanya737@users.noreply.github.com>
2024-01-17 18:09:24 +0700

    Update DataFrame.py

--- DataFrame.py
+++ DataFrame.py
@@ -5,13 +5,29 @@ data = pd.read_csv("data.csv", delimiter=";",
 print(data)
 
 def job():
-    Enter_1 = str(input("Enter сolumn name"))
-    Enter_2 = int(input("Enter cell number"))
 
-    print(data.at [Enter_2, Enter_1])
+    try:
+
+        Enter_1 = str(input("Enter сolumn name"))
+        Enter_2 = int(input("Enter cell number"))
+
+        print(data.at [Enter_2, Enter_1])
+
+    except ValueError:
+        print("Oops! You were hurt as a child")
+
+    except NameError:
+        print("Oops! You were hurt as a child")
+
+    except TypeError:
+        print("Oops! You were hurt as a child")
+
+    except KeyError:
+        print("Oops! You were hurt as a child")
 
 
 job()
+
 
 
 
